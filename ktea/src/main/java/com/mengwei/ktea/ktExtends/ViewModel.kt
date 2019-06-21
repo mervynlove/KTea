14d74ddd8e56721693ddd54fbe7da870278ca71e
mervynlove <mervynlove@qq.com>
2019-06-21 16:09:29 +0800

    update 1.1.6

--- ktea/src/main/java/com/mengwei/ktea/ktExtends/ViewModel.kt
+++ ktea/src/main/java/com/mengwei/ktea/ktExtends/ViewModel.kt
@@ -8,10 +8,11 @@ import android.arch.lifecycle.Observer
  * Create by MengWei at 2018/9/29
  */
 
+/*
 fun <T : Any> LiveData<T>.observe(owner: LifecycleOwner, observer: (T) -> Unit) {
     observe(owner, Observer {
         it?.run {
             observer(this)
         }
     })
-}
+}*/
